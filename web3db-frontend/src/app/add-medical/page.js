871af843c066d64619342c514602a0766af532f7
wayne-617 <wgl91603@uga.edu>
2024-11-12 01:54:06 -0500

    Updated medical form

--- web3db-frontend/src/app/add-medical/page.js
+++ web3db-frontend/src/app/add-medical/page.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import Form from '../components/Form'
 
 const page = () => {
   return (
     
     <div>
       <Navbar />
-      Add Medical Info
+      <h1>Add Medical Data</h1>
+      <Form />
     </div>
   )
 }
--- web3db-frontend/src/app/components/Form.js
+++ web3db-frontend/src/app/components/Form.js
@@ -1,17 +1,49 @@
+'use client';
 import React, { useState } from 'react'
 
-export const Form = () => {
+const Form = () => {
 
     const[form, setForm] = useState({data: ''});
+    const[submitting, setSubmitting] = useState(false)
 
-    const handleUpload = (e)
+    const handleChange = (e) => {
+        setForm({...form, data: e.target.value})
+        console.log(form.data)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        setSubmitting(true)
+        console.log("Submitted")
+
+        // Insert API Post
+
+        setSubmitting(false)
+    }
 
     return (
         <div>
-            <h2>Add Medical Data</h2>
-            <form onSubmit>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label>Enter Data:</label>
+                    <input 
+                        type="text"
+                        value={form.data}
+                        onChange={handleChange}
+                        required
+                    />
+                </div>
+
+                <div>
+                    <button type='submit' disabled={submitting}>
+                        {submitting ? 'Submitting...' : 'Submit'}
+                    </button>
+                </div>
 
             </form>
+            
         </div>
     )
 }
+
+export default Form
